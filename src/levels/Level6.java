305e0d6abbec2f8e87e355dbd3321fb2f4de3b5b
Tạ Đình Nguyên <24021587@vnu.edu.vn>
2025-11-03 15:44:10 +0700

    Sửa lại level

--- src/levels/Level6.java
+++ src/levels/Level6.java
@@ -1,14 +1,24 @@
 package levels;
 
 public class Level6 {
+    // Hard mode: dense multi-hit with undestructable (X) chokepoints
+    // Each row has 15 columns to avoid scaling artifacts.
     public static final String[] MAP = new String[]{
-        "333333333333",
-        "333333333333",
-        "222222222222",
-        "222222222222",
-        "111111111111",
-        "111111111111",
-        "111111111111",
-        "111111111111",
+        "333X333X333X333",
+        "33XXX33XXX33XXX",
+        "X333333X333333X",
+        "33X33333X333X33",
+        "333333333333333",
+        "XX3XX3XXXXX3XX3",
+        "333233333333233",
+        "3X3X3X3X3X3X3X3",
+        "333333X33333333",
+        "XX33333X33333XX",
+        "333X3333333X333",
+        "33XXX33X33XXX33",
+        "333333333333333",
+        "X3X3X3X3X3X3X3X",
+        "333233333333233",
+        "XXXXXXXX3XXXXXX",
     };
 }
